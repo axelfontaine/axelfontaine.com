bada402a0b118a6c36d3bdacd85d1badd60eefeb
Axel Fontaine <business@axelfontaine.com>
2015-01-11 11:52:44 +0100

    Updated to fromspree

--- assets/axelfontaine.js
+++ assets/axelfontaine.js
@@ -49,7 +49,7 @@ function showContactPopup(subject) {
 
 function postContactForm() {
     $.ajax({
-        url: "//forms.brace.io/axel@boxfuse.com",
+        url: "//formspree.io/axel@boxfuse.com",
         method: "POST",
         data: {
             "_replyto": $('#entry_1364296767').val(),
